13d122bb148ef3eea2966a7c24bf839e30765a52
ds <dskim.logiall@gmail.com>
2022-04-25 23:15:36 +0900

    컨트롤러 및 콘솔 뷰어 기능 추가

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,2 +1,69 @@
-package racingcar.controller;public class RacingController {
+package racingcar.controller;
+
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Racing;
+import racingcar.util.CarUtil;
+import racingcar.view.ConsoleView;
+
+public class RacingController {
+
+    public void play() {
+
+        ConsoleView.inputCarNames();
+        String carNames = inputCarNames();
+
+        ConsoleView.inputRound();
+        String round = inputRound();
+
+        Racing racing = new Racing(carNames, round);
+
+        ConsoleView.printResultInfo();
+
+        for (int i = 0; i < racing.getRound(); i++) {
+            ConsoleView.printRoundResult(racing.move());
+        }
+
+        ConsoleView.printWinner(racing.getWinner());
+    }
+
+    public String inputCarNames() {
+        String carNames = "";
+
+        while (true) {
+            carNames = Console.readLine();
+            try {
+                CarUtil.validateCarNames(carNames);
+            } catch (IllegalArgumentException e) {
+                carNames = "";
+                ConsoleView.printError(e.getMessage());
+            }
+
+            if (!"".equals(carNames)) {
+                break;
+            }
+        }
+
+        return carNames;
+    }
+
+    public String inputRound() {
+        String round = "";
+
+        while (true) {
+            round = Console.readLine();
+            try {
+                CarUtil.validateRound(round);
+            } catch (IllegalArgumentException e) {
+                ConsoleView.printError(e.getMessage());
+                round = "";
+            }
+
+            if (!"".equals(round)) {
+                break;
+            }
+        }
+
+        return round;
+    }
+
 }
