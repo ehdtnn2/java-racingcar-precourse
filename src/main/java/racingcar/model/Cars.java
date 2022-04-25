bcc83786c57759622625db4f2aba60c59f9c7acb
ds <dskim.logiall@gmail.com>
2022-04-25 22:36:59 +0900

    feat: car, cars model 기능 및 테스트

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,2 +1,54 @@
-package racingcar.model;public class Cars {
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+
+    public static final int CARS_SIZE_MIN = 2;
+
+    private List<Car> cars;
+
+    public Cars(String input) {
+        validateCarNames(input);
+
+        cars = new ArrayList<>();
+
+        String[] carNames = input.split(",");
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
+
+    private void validateCarNames(String input) {
+        String[] carNames = input.split(",");
+        if (carNames.length < CARS_SIZE_MIN) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void move() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
+
+    public int size() {
+        return cars.size();
+    }
+
+    public Car get(int i) {
+        return cars.get(i);
+    }
+
+    public int getMaxMileage() {
+        int max = 0;
+        for (Car car : cars) {
+            int mileage = car.getMileage();
+            if (max <= mileage) {
+                max = mileage;
+            }
+        }
+        return max;
+    }
 }
