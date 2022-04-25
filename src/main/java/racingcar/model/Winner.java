8605f52a7ec011fa7fa63b4fe9408a513353f046
ds <dskim.logiall@gmail.com>
2022-04-25 23:14:20 +0900

    feat: 자동차 게임 우승자 로직 개발 및 우승자 도메인 추가

--- src/main/java/racingcar/model/Winner.java
+++ src/main/java/racingcar/model/Winner.java
@@ -1,2 +1,24 @@
-package racingcar.model;public class Winner {
+package racingcar.model;
+
+import java.util.List;
+
+public class Winner {
+
+    private List<String> names;
+
+    public Winner(List<String> names) {
+        this.names = names;
+    }
+
+    public boolean isSolo() {
+        return names.size() == 1;
+    }
+
+    public int size() {
+        return names.size();
+    }
+
+    public String get(int i) {
+        return names.get(i);
+    }
 }
