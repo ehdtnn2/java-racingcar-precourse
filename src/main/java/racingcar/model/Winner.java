2d919fdb6624a5c042123f21ca0d1f6897b4c238
ds <dskim.logiall@gmail.com>
2022-04-25 23:29:20 +0900

    feat: winner validate and test

--- src/main/java/racingcar/model/Winner.java
+++ src/main/java/racingcar/model/Winner.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class Winner {
 
+    public static final String INVALID_SIZE = "우승자 미입력 에러";
+
     private List<String> names;
 
     public Winner(List<String> names) {
         this.names = names;
+        validateWinnerSize();
+    }
+
+    private void validateWinnerSize() {
+        if (this.names.size() < 1) {
+            throw new IllegalArgumentException(INVALID_SIZE);
+        }
     }
 
     public boolean isSolo() {
